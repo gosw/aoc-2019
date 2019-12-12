f960fe4bb22cda9263f1e2a7aa51cc91b2923e88
gosw <gosw@users.noreply.github.com>
2019-12-12 21:41:49 +0100

    hopefully last time

--- day01/csharp/gosw/solutionDay1/Program.cs
+++ day01/csharp/gosw/solutionDay1/Program.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace solutionDay1
+﻿namespace solutionDay1
 {
     class Program
     {
         static void Main(string[] args)
         {
+
         }
     }
 }

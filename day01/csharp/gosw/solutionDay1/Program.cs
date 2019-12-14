cfbdbf8b6f27c6215d80d44339a2e1d3f994d7da
gosw <gosw@users.noreply.github.com>
2019-12-14 15:28:44 +0100

    day1 done

--- day01/csharp/gosw/solutionDay1/Program.cs
+++ day01/csharp/gosw/solutionDay1/Program.cs
@@ -1,10 +1,62 @@
-﻿namespace solutionDay1
+﻿using System;
+using System.IO;
+
+namespace solutionDay1
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var fuelSum = 0;
+            try
+            {
+                using (var input = new StreamReader("input.txt"))
+                {
+                    var line = "";
+                    while ((line = input.ReadLine()) != null)
+                    {
+                        fuelSum += fuelForModule(line);
+                    }
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.GetType());
+                Console.WriteLine(e.Message);
+            }
 
+            Console.WriteLine("Fuel needed: " + fuelSum);
+            Console.ReadLine();
+        }
+
+        private static int fuelForModule(string moduleMass)
+        {
+            var fuelAmount = 0;
+            if (Int32.TryParse(moduleMass, out fuelAmount))
+            {
+                fuelAmount = (fuelAmount / 3) - 2;
+                if (fuelAmount >= 0)
+                {
+                    fuelAmount += fuelForFuel(fuelAmount);
+                }
+            }
+
+            return fuelAmount;
+        }
+
+        private static int fuelForFuel(int fuelAmount)
+        {
+            var additionalFuel = 0;
+            var fuelWeight = fuelAmount;
+
+            do
+            {
+                var currentFuelWeight = (fuelWeight / 3) - 2;
+                fuelWeight = (currentFuelWeight < 0) ? 0 : currentFuelWeight;
+                additionalFuel += fuelWeight;
+            } while (fuelWeight > 0);
+
+            return additionalFuel;
         }
     }
 }

a3abd10496f11d75118c463605bab8f0a132832f
gosw <gosw@users.noreply.github.com>
2020-01-01 21:16:52 +0100

    day 2 part 2 done

---
+++ day02/solutionDay2/Computer.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace csharp.gosw.solutionDay2
+{
+    public class Computer : IDisposable
+    {
+        // Flag: Has Dispose already been called?
+        bool disposed = false;
+        protected int[] Program { get; private set; }
+
+        public void Dispose()
+        {
+            // Dispose of unmanaged resources.
+            Dispose(true);
+            // Suppress finalization.
+            GC.SuppressFinalize(this);
+        }
+
+        private void Dispose(bool disposing)
+        {
+            if (disposed)
+                return;
+
+            if (disposing)
+            {
+                Dispose(disposed);
+                // Free any other managed objects here.
+            }
+
+            disposed = true;
+        }
+
+        public Computer Instance (int[] program)
+        {
+            return new Computer { Program = program };
+        }
+
+        public int RunProgram(int noun, int verb)
+        {
+            Program[1] = noun;
+            Program[2] = verb;
+
+            for (int i = 0; i < Program.Length; i += 4)
+            {
+                switch (Program[i])
+                {
+                    case 99:
+                        break;
+
+                    case 1:
+                        Program[Program[i + 3]] = Program[Program[i + 1]] + Program[Program[i + 2]];
+                        break;
+
+                    case 2:
+                        Program[Program[i + 3]] = Program[Program[i + 1]] * Program[Program[i + 2]];
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+            return Program[0];
+        }
+    }
+}
--- day02/solutionDay2/Program.cs
+++ day02/solutionDay2/Program.cs
@@ -8,14 +8,15 @@ namespace csharp.gosw.solutionDay2
         {
             int[] codes = { 1, 0, 0, 3, 1, 1, 2, 3, 1, 3, 4, 3, 1, 5, 0, 3, 2, 1, 13, 19, 1, 10, 19, 23, 2, 9, 23, 27, 1, 6, 27, 31, 1, 10, 31, 35, 1, 35, 10, 39, 1, 9, 39, 43, 1, 6, 43, 47, 1, 10, 47, 51, 1, 6, 51, 55, 2, 13, 55, 59, 1, 6, 59, 63, 1, 10, 63, 67, 2, 67, 9, 71, 1, 71, 5, 75, 1, 13, 75, 79, 2, 79, 13, 83, 1, 83, 9, 87, 2, 10, 87, 91, 2, 91, 6, 95, 2, 13, 95, 99, 1, 10, 99, 103, 2, 9, 103, 107, 1, 107, 5, 111, 2, 9, 111, 115, 1, 5, 115, 119, 1, 9, 119, 123, 2, 123, 6, 127, 1, 5, 127, 131, 1, 10, 131, 135, 1, 135, 6, 139, 1, 139, 5, 143, 1, 143, 9, 147, 1, 5, 147, 151, 1, 151, 13, 155, 1, 5, 155, 159, 1, 2, 159, 163, 1, 163, 6, 0, 99, 2, 0, 14, 0 };
 
-            codes[1] = 12;
-            codes[2] = 2;
+            /**For Part 1
+            //codes[1] = 12;
+            //codes[2] = 2;
 
             for (int i = 0; i < codes.Length; i+=4)
             {
                 switch (codes[i])
                 {
-                    case 99: //continue;
+                    case 99:
                         break;
 
                     case 1:
@@ -39,9 +40,28 @@ namespace csharp.gosw.solutionDay2
                     default:
                         break;
                 }
+            }*/
+
+            int[] currentProgram = new int[173];
+
+            for (int i = 0; i < 99; i++)
+            {
+                for (int j = 0; j < 99; j++)
+                {
+                    Array.Copy(codes, currentProgram, codes.Length);
+                    using (var computer = new Computer().Instance(currentProgram))
+                    {
+                        if (computer.RunProgram(i, j) == 19690720)
+                        {
+                            Console.WriteLine("Output: " + ((100 * i) + j));
+                            Console.ReadLine();
+                            break;
+                        }
+                    }
+                } 
             }
-            Console.WriteLine("Output: " + codes[0]);
-            Console.ReadLine();
+            Console.WriteLine("Not found");
+            Console.ReadLine();   
         }
     }
 }
